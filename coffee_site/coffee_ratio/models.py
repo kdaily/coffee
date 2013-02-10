6dc0bac15418e28e1ee00583bb9321e70acb99dd
Kenneth Daily <kmdaily@gmail.com>
2013-02-10 11:37:08 -0500

    Added equipment class.

---
+++ coffee_site/coffee_ratio/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Equipment(models.Model):
+    pass
+    # poll = models.ForeignKey(Poll)
+    # choice = models.CharField(max_length=200)
+    # votes = models.IntegerField()
