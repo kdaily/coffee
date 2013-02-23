6a4242fa2a69524cf99ccf000a235a1c25157653
Kenneth Daily <kmdaily@gmail.com>
2013-02-23 17:40:30 -0500

    Updated urls.

--- coffee_site/coffee_site/urls.py
+++ coffee_site/coffee_site/urls.py
@@ -9,7 +9,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'coffee_site.views.home', name='home'),
     # url(r'^coffee_site/', include('coffee_site.foo.urls')),
 
+    url(r'^base/$', 'base.views.index'),
+    url(r'^base/coffees/$', 'coffee_journal.views.index'),
+    url(r'^base/coffee/(?P<coffee_id>\d+)/$', 'coffee_journal.views.coffee_detail'),
+
     url(r'^coffee_journal/coffees/$', 'coffee_journal.views.index'),
+    url(r'^coffee_journal/coffee/(?P<coffee_id>\d+)/$', 'coffee_journal.views.coffee_detail'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
