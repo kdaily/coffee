3a7f4362cc9b93687e6d3afef8af4aaa7ee3f0da
Kenneth Daily <kmdaily@gmail.com>
2013-07-13 15:42:39 -0400

    Added register view.

--- coffee_site/base/views.py
+++ coffee_site/base/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the base index.")
@@ -11,4 +15,14 @@ def index(request):
 def logout_view(request):
     logout(request)
 
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = UserCreationForm()
 
+    return render(request, "registration/register.html", 
+                  {'form': form,})
