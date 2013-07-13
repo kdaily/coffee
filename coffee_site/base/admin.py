ceaaf618d800b029f7f1e38f8bd740a324fbd204
Kenneth Daily <kmdaily@gmail.com>
2013-07-13 17:24:42 -0400

    Added models to admin page.

--- coffee_site/base/admin.py
+++ coffee_site/base/admin.py
@@ -1,20 +1,26 @@
-from base.models import Coffee
+from base.models import Coffee, CoffeeBag, PurchasedCoffeeBag, Roaster, Store
 from django.contrib import admin
 # from django.contrib.auth.admin import UserAdmin
 # from django.contrib.auth.models import User
 
 
-class CoffeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'finca', 'date_purch', 'date_roast', 'purch_location', 'date_roast', 'amount', 'roaster')
+# class CoffeeAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'finca', 'date_purch', 'date_roast', 'purch_location', 'date_roast', 'amount', 'roaster')
 
 
-    fieldsets = [
-        ('Coffee',               {'fields': ['name', 'finca', 'grower', 'varietal', 'altitude', 'notes']}),
-        ('Roaster',               {'fields': ['roaster', 'amount', 'date_roast']}),
-        ('Purchase', {'fields': ['date_purch', 'purch_location'], 'classes': ['collapse']})
-        ]
+#     fieldsets = [
+#         ('Coffee',               {'fields': ['name', 'finca', 'grower', 'varietal', 'altitude', 'notes']}),
+#         ('Roaster',               {'fields': ['roaster', 'amount', 'date_roast']}),
+#         ('Purchase', {'fields': ['date_purch', 'purch_location'], 'classes': ['collapse']})
+#         ]
 
-admin.site.register(Coffee, CoffeeAdmin)
+# admin.site.register(Coffee, CoffeeAdmin)
+
+admin.site.register(Coffee)
+admin.site.register(Roaster)
+admin.site.register(Store)
+admin.site.register(CoffeeBag)
+admin.site.register(PurchasedCoffeeBag)
 
 
 # Stuff to have a new user class admin
