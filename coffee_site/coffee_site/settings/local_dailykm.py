8ad761a27229c11f8fb0ebd0d41f4fc7ad2ebf3f
Kenneth Daily <kmdaily@gmail.com>
2013-07-14 11:38:19 -0400

    Added db settings.

--- coffee_site/coffee_site/settings/local_dailykm.py
+++ coffee_site/coffee_site/settings/local_dailykm.py
@@ -6,3 +6,14 @@ from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'djangocoffee',     # Or path to database file if using sqlite3.
+        'USER': 'dailykm',                      # Not used with sqlite3.
+        'PASSWORD': '2Sr3x99g',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
