418d41975820f3acb30f2769e4e687ce2a3229ad
Kenneth Daily <kmdaily@gmail.com>
2013-07-14 13:43:10 -0400

    Added PurchasedCoffeeBag model.

--- coffee_site/coffee_journal/models.py
+++ coffee_site/coffee_journal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+
+from djangoratings.fields import RatingField
+
+from base.models import Store, CoffeeBag
 
 # Create your models here.
+
+class PurchasedCoffeeBag(models.Model):
+
+    # purch_location = models.CharField(max_length=500)
+    date_purch = models.DateField('Purchase Date', blank=True)
+
+    rating = RatingField(range=5)
+
+    user = models.ManyToManyField(User)
+    store = models.ForeignKey(Store)
+    coffeebag = models.ForeignKey(CoffeeBag)
+
+    def __unicode__(self):
+        return "%s, %s, %s" % (self.coffeebag.coffee.name, self.coffeebag.roaster.name, self.coffeebag.store.name)
