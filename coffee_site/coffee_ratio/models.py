0cd4a0d690b19eb4e4188af954964e002e6e38fd
Vessy <vesna.memisevic@gmail.com>
2013-07-14 10:49:56 -0400

    Added tables for coffee grinders and coffee makers

--- coffee_site/coffee_ratio/models.py
+++ coffee_site/coffee_ratio/models.py
@@ -15,7 +15,48 @@ class Method(models.Model):
     rec_grind = models.CharField(max_length=100, blank=True, null=True)
     rec_water_amt = models.IntegerField(blank=True, null=True)
     rec_coffee_amt = models.IntegerField(blank=True, null=True)
+      
+    
+class CoffeeMaker(models.Model):
 
+    name = models.CharField(max_length=500)
+    manufacturer = models.CharField(max_length=500)
+    model = models.CharField(max_length=500)
+    volume = models.FloatField(blank=True, null=True)
+    
+    description = models.TextField(blank=True, null=True)
+    thumb = ImageField(upload_to='/media/img/', blank=True)
+
+    method = models.OneToOneField(Method)    
+    
+class CoffeeGrinder(models.Model):
+
+    name = models.CharField(max_length=500)
+    manufacturer = models.CharField(max_length=500)
+    model = models.CharField(max_length=500)
+    description = models.TextField(blank=True, null=True)
+    thumb = ImageField(upload_to='/media/img/', blank=True)
+    
+    
+    
+class PurchasedCoffeeMaker(models.Model):
+
+    # should be changed to only a year
+    date_purch = models.DateField('Purchase Date', blank=True)
+    rating = RatingField(range=5)
+    user = models.ManyToManyField(User)    
+    coffeemaker = models.ForeignKey(CoffeeMaker)
+
+
+class PurchasedCoffeeGrinder(models.Model):
+
+    # should be changed to only a year
+    date_purch = models.DateField('Purchase Date', blank=True)
+    rating = RatingField(range=5)
+    user = models.ManyToManyField(User)    
+    coffeegrinder = models.ForeignKey(CoffeeGrinder)
+    
+    
 class Preparation(models.Model):
 
     date = models.DateField('Preparation Date', blank=True)
@@ -27,7 +68,8 @@ class Preparation(models.Model):
     notes = models.TextField(blank=True, null=True)
     rating = RatingField(range=5)
 
-    method = models.ForeignKey(Method)
+    method = models.ForeignKey(PurchasedCoffeeMaker)
+    grinder = models.ForeignKey(PurchasedCoffeeGrinder)
     coffeebag = models.ForeignKey(PurchasedCoffeeBag)
     user = models.ForeignKey(User)
 
