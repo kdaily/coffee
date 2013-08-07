b01fb17f44f09776010ae295bb57d57535b4977d
Kenneth Daily <kmdaily@gmail.com>
2013-08-07 12:00:47 -0400

    Added comments.
    Also, removed duplicate views that are in base.views.

--- coffee_site/coffee_journal/views.py
+++ coffee_site/coffee_journal/views.py
@@ -16,8 +16,19 @@ from .models import PurchasedCoffeeBag, PurchasedCoffeeBagForm
 # Create your views here.
 
 def login(request):
+    """User login.
+    
+    Handles successful/unsuccessful logins.
+    
+    This was meant to be specific to the coffee journal portion
+    of the app, but probably could be used site-wide (coffee ratio too)
+    
+    """
+    
     state = "Please log in below..."
-    username = password = ''
+    username = ''
+    password = ''
+    
     if request.POST:
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -36,34 +47,44 @@ def login(request):
             state = "Your username and/or password were incorrect."
 
     return render_to_response('coffee_journal/login.html',
-                              {'state':state, 'username': username},
+                              {'state': state, 'username': username},
                               context_instance=RequestContext(request))
 
 @login_required
 def home(request):
+    """Display the home (index) page.
+    
+    Shouldn't need a login required, and needs to handle when
+    users are not logged in.
+    
+    """
+    
     return render_to_response('coffee_journal/index.html',
                               context_instance=RequestContext(request))
 
-# @login_required
-# def coffee_carousel(request):
-#     return render_to_response('coffee_journal/coffee_carousel.html',
-#                               context_instance=RequestContext(request))
-
 def logout(request):
+    """Log users out and re-direct them to the login page.
+    
     """
-    Log users out and re-direct them to the main page.
-    """
+    
     auth.logout(request)
 
-    # return HttpResponseRedirect('/login/')
     return redirect('coffee_journal.views.login')
 
 class PurchasedCoffeeBagDetailView(DetailView):
+    """Detail view for purchased coffee bags.
+    
+    """
+    
     model = PurchasedCoffeeBag
     template_name = 'coffee_journal/purchasedcoffeebag_detail.html'
 
 class PurchasedCoffeeBagCreateView(LoginRequiredMixin, CreateView):
-    model = PurchasedCoffeeBag
+    """View to add new purchased coffee bags.
+    
+    """
+
+model = PurchasedCoffeeBag
     form_class = PurchasedCoffeeBagForm
 
     template_name = 'coffee_journal/purchasedcoffeebag_create.html'
@@ -71,15 +92,28 @@ class PurchasedCoffeeBagCreateView(LoginRequiredMixin, CreateView):
     context_object_name = 'purch_coffee_create'
 
     def get(self, request, *args, **kwargs):
+        """Handle GET requests.
+        
+        Display the form; initializes the user variable to the currently logged
+        in user.
+        
+        """
+        
         curruser = User.objects.get(pk=request.user.id)
         form = self.form_class(initial={'id_user': curruser})
-        ## form = self.form_class()
+        
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
-
+        """Handle POST requests.
+        
+        Process the form. Currently uses default validation, could probably
+        use some custom validation.
+        
+        """
+        
         form = self.form_class(request.POST)
-
+        
         if form.is_valid():
             cmodel = form.save()
             #This is where you might chooose to do stuff.
@@ -89,74 +123,57 @@ class PurchasedCoffeeBagCreateView(LoginRequiredMixin, CreateView):
         
         return render(request, self.template_name, {'form': form})
 
-
 class PurchasedCoffeeBagListView(ListView):
-
+    """View for a list of purchased coffee bags.
+        
+    """
+    
     model = PurchasedCoffeeBag
     template_name = 'coffee_journal/purchasedcoffeebag_list.html'
     paginate_by = 8 
     context_object_name = 'purchasedcoffeebag_list'
 
-class UserPurchasedCoffeeBagListView(ListView):
-
+class UserPurchasedCoffeeBagListView(LoginRequiredMixin, ListView):
+    """View a paginated list of purchased coffee bags for the logged in user.
+    
+    """
+    
     model = PurchasedCoffeeBag
     template_name = 'coffee_journal/purchasedcoffeebag_list.html'
     paginate_by = 5
 
     def get_queryset(self):
+        """Get list of purchased coffee bags from the DB for the currently logged in user.
+        
+        """
+        
+        # Get the current user, or return a 404 error
         self.user = get_object_or_404(User, username=self.args[0])
+        
         return PurchasedCoffeeBag.objects.filter(user__id=self.user.id).order_by('-date_purch')
 
 class SearchPurchasedCoffeeBagListView(ListView):
-
+    """Testing example - search the purchased coffee bags for keyword in varietal column.
+    
+    This is a bad and slow way to implement searching; needs a dedicated indexed search functionality.
+    There are many Django packages that provide this.
+    
+    """
+    
     model = PurchasedCoffeeBag
     template_name = 'coffee_journal/purchasedcoffeebag_list.html'
     paginate_by = 5
-
+    
     def get_queryset(self):
+        """Override the queryset to search for keyword in varietal column.
+        
+        """
+        
+        # Default way to get the query set
         queryset = super(SearchPurchasedCoffeeBagListView, self).get_queryset()
         
+        # Get the query filter parameters
+        # Currently only searches the varietal label
         q = self.request.GET.get("q")
+        
         return queryset.filter(varietal__icontains=q)
-    
-# @login_required
-# def purchased_coffees_paginated(request):
-#     latest_coffee_list = PurchasedCoffeeBag.objects.filter(user__id=request.user.id).order_by('-date_purch')
-#     paginator = Paginator(latest_coffee_list, 5)
-
-#     t = loader.get_template('coffee_journal/purchased_coffees_paginated.html')
-
-#     page = request.GET.get('page')
-    
-#     try:
-#         coffees = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer, deliver first page.
-#         coffees = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range (e.g. 9999), deliver last page of results.
-#         coffees = paginator.page(paginator.num_pages)
-
-#     c = RequestContext(request, {'latest_coffee_list': coffees, 'user': request.user})
-#     return HttpResponse(t.render(c))
-
-class CoffeeDetailView(DetailView):
-    model = Coffee
-    template_name = 'base/coffee_detail.html'
-
-class CoffeeBagDetailView(DetailView):
-    model = CoffeeBag
-    template_name = 'base/coffeebag_detail.html'
-
-class PurchasedCoffeeBagDetailView(DetailView):
-    model = PurchasedCoffeeBag
-    template_name = 'coffee_journal/purchasedcoffeebag_detail.html'
-
-# def purchased_coffee_detail(request, coffee_id):
-#     coffee = PurchasedCoffeeBag.objects.get(pk=coffee_id)
-
-#     t = loader.get_template('coffee_journal/purchased_coffee_detail.html')
-#     c = RequestContext(request, {'coffee': coffee})
-
-#     return HttpResponse(t.render(c))
-
