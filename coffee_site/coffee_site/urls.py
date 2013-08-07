b3cad17c765bb537422314b59db440fc77774407
Kenneth Daily <kmdaily@gmail.com>
2013-08-07 12:04:30 -0400

    Added comments.

--- coffee_site/coffee_site/urls.py
+++ coffee_site/coffee_site/urls.py
@@ -22,53 +22,66 @@ urlpatterns = patterns('',
                        
                        # url(r'^login/$', 'django.contrib.auth.views.login'),                       
                        ## url(r'^logout/$', 'django.contrib.auth.views.logout'),                       
-                                              
+                       
+                       # Main page
                        url(r'^$', 'coffee_journal.views.home', name='home'),
 
-                       url(r'^register/$', 'base.views.register'),                       
+                       # Register/login/logout of the coffee jounal
+                       # Probably could all be moved to base.
                        url(r'^login/$', 'coffee_journal.views.login'),                       
                        url(r'^logout/$', 'coffee_journal.views.logout'),
 
+                       # View detail for a coffee
                        url(r'^coffee/(?P<pk>\d+)/$', 
                            view=CoffeeDetailView.as_view(),
                            name="coffeedetail"),
 
+                       # View detail for a coffee bag
                        url(r'^coffeebag/(?P<pk>\d+)/$', 
                            view=CoffeeBagDetailView.as_view(),
                            name="coffeebagdetail"),
 
+                       # View detail for a purchased coffee bag
                        url(r'^purchcoffeebag/(?P<pk>\d+)/$', 
                            view=PurchasedCoffeeBagDetailView.as_view(),
                            name="purchcoffeebagdetail"),
 
+                       # View list of purchased coffee bags
                        url(r'^purchased_coffees/$', 
                            view=PurchasedCoffeeBagListView.as_view(),
                            name="purchcoffeebaglist"),
 
+                       # View list of purchased coffee bags for a particular user
                        url(r'^purchased_coffees/([A-Za-z]+[\w-]*)/$', 
                            view=UserPurchasedCoffeeBagListView.as_view(),
                            name="userpurchcoffeebaglist"),
 
+                       # Create a new purchased coffee bags
                        url(r'^addpurchcoffeebag/$', 
                            view=PurchasedCoffeeBagCreateView.as_view(),
                            name="purchcoffeebagcreate"),
 
+                       # Create a new coffee 
                        url(r'^addcoffee/$', 
                            view=CoffeeCreateView.as_view(),
                            name="coffeecreate"),
 
+                       # View a list of coffees
                        url(r'^coffees/$', 
                            view=CoffeeListView.as_view(),
                            name="coffeelist"),
 
+                       # View a list of coffee bags
                        url(r'^coffeebags/$', 
                            view=CoffeeBagListView.as_view(),
                            name="coffeebaglist"),
 
+                       # View a list of roasters
                        url(r'^roasters/$', 
                            view=RoasterListView.as_view(),
                            name="roasterlist"),
 
+                       # View details for roaster by primary key
                        url(r'^roaster/(?P<pk>\d+)/$', 
                            view=RoasterDetailView.as_view(),
                            name="roasterdetail"),
