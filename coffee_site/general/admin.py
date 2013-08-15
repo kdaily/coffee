460acae355faeef371d918042fa01ffac6425227
Kenneth Daily <kmdaily@gmail.com>
2013-08-14 20:30:46 -0400

    Removed commented code for user admin.

--- coffee_site/general/admin.py
+++ coffee_site/general/admin.py
@@ -1,9 +1,8 @@
-from general.models import Coffee, CoffeeBag, Roaster, Store
 from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin
-# from django.contrib.auth.models import User
 
 from guardian.admin import GuardedModelAdmin
+
+from .models import Coffee, CoffeeBag, Roaster, Store
 
 # Old way:
 #class AuthorAdmin(admin.ModelAdmin):
@@ -19,20 +18,3 @@ admin.site.register(Coffee, CoffeeAdmin)
 admin.site.register(Roaster)
 admin.site.register(Store)
 admin.site.register(CoffeeBag)
-
-
-# Stuff to have a new user class admin
-# Not using the new user class now...
-
-# class CoffeeUserInline(admin.StackedInline):
-#     model = CoffeeUser
-#     can_delete = False
-#     verbose_name_plural = 'coffeeusers'
-
-# # Define a new User admin
-# class UserAdmin(UserAdmin):
-#     inlines = (CoffeeUserInline, )
-
-# # # Re-register UserAdmin
-# admin.site.unregister(User)
-# admin.site.register(User, UserAdmin)
