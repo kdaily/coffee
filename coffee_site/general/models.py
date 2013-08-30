c7cfef1c81fff46d2b189eb398796649e00b3c0c
Vessy <vesna.memisevic@gmail.com>
2013-08-29 21:45:34 -0400

    changed definition of a CoffeeBag, add CoffeeRoaster table, add NewsInfo table

--- coffee_site/general/models.py
+++ coffee_site/general/models.py
@@ -91,18 +91,12 @@ class Coffee(models.Model):
         # the columns that make unique records
         unique_together = ("name", "grower", "finca")
     
-class CoffeeBag(models.Model):
+class CoffeeRoaster(models.Model):
     """DB model for a coffee bag.
 
     This has a roaster and a coffee as relationships.
 
     """
-
-    date_roast = models.DateField('Roast Date', blank=True, null=True)
-    
-    amount = models.FloatField(blank=True, null=True)
-    price = models.FloatField(blank=True, null=True)
-    
     roast_type = models.CharField(max_length=200, blank=True, null=True)
 
     #To use ImageFields, you need to install the Python Imaging Library
@@ -114,11 +108,37 @@ class CoffeeBag(models.Model):
     rating = RatingField(range=5)
     
     def __unicode__(self):
-        return "%s, %s (%s)" % (self.coffee.name, self.roaster.name, self.date_roast)
+        return "%s, %s (%s)" % (self.coffee.name, self.roaster.name)
 
     class Meta:
         # the columns that make unique records
-        unique_together = ('roaster', 'coffee', 'date_roast')
+        unique_together = ('roaster', 'coffee')
+    
+class CoffeeBag(models.Model):
+    """DB model for a specific coffee bag.
+
+    This has a roaster and a coffee as relationships.
+
+    """
+    #Maybe we should only have a year?
+    date_roast = models.DateField('Roast Date', blank=True, null=True)
+    
+    amount = models.FloatField(blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    
+    #To use ImageFields, you need to install the Python Imaging Library
+    thumb = models.ImageField(upload_to='/media/img/', blank=True)
+
+    coffee_roaster = models.ForeignKey(CoffeeRoaster)
+
+    rating = RatingField(range=5)
+    
+    def __unicode__(self):
+        return "%s, %s (%s)" % (self.coffee_roaster.coffee, self.coffee_roaster.roaster, self.date_roast)
+
+    class Meta:
+        # the columns that make unique records
+        unique_together = ('coffee_roaster', 'date_roast')
         
 class AromaTaste(models.Model):
     """Model for a aroma/taste wheel
@@ -176,9 +196,23 @@ class RoasterStore(models.Model):
 
     store = models.ManyToManyField(Store)
     roaster = models.ForeignKey(Roaster)
-    coffeeBag = models.ForeignKey(CoffeeBag)
+    coffee_roaster = models.ForeignKey(CoffeeRoaster)
     
-        
+    
+class NewsInfo(models.Model):
+    """news for the first page.
+    
+    """
+    date_post = models.DateField('Date', blank=True)
+    title = models.CharField(max_length=500)
+    text = models.TextField(blank=True, null=True)   
+    source = models.CharField(max_length=500, blank=True, null=True)
+    website = models.CharField(max_length=500, blank=True, null=True)
+    thumb = models.ImageField(upload_to='/media/img/', blank=True)
+    posted_by = models.ManyToManyField(User)
+    
+    
+            
 def make_custom_datefield(f):
     """Change the format of the date in form fields.
     
