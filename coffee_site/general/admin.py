d22bbf505ac9b5a685a033c7ea04720ce871dc79
Kenneth Daily <kmdaily@gmail.com>
2013-09-02 22:17:38 -0400

    added guarded model admins.

--- coffee_site/general/admin.py
+++ coffee_site/general/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from guardian.admin import GuardedModelAdmin
 
-from .models import Coffee, CoffeeBag, RoastedCoffeeBag, 
+from .models import Coffee, CoffeeBag, RoastedCoffeeBag
 from .models import Roaster, UserRoaster
 from .models import Store, UserStore
 
@@ -14,12 +14,18 @@ from .models import Store, UserStore
 class CoffeeAdmin(GuardedModelAdmin):
     pass
 
+class UserRoasterAdmin(GuardedModelAdmin):
+    pass
+
+class UserStoreAdmin(GuardedModelAdmin):
+    pass
+
 admin.site.register(Coffee, CoffeeAdmin)
 ## admin.site.register(Coffee)
 
 admin.site.register(Roaster)
-admin.site.register(UserRoaster)
+admin.site.register(UserRoaster, UserRoasterAdmin)
 admin.site.register(Store)
-admin.site.register(UserStore)
+admin.site.register(UserStore, UserStoreAdmin)
 admin.site.register(CoffeeBag)
 admin.site.register(RoastedCoffeeBag)
