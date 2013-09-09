c38d13792566f33ae06af19435c1cafd3202f416
Kenneth Daily <kmdaily@gmail.com>
2013-09-08 21:38:29 -0400

    Added fields to fieldset in the CoffeeUser admin.
    Also added a new form to use (this was required to get the new fields
    to show in the admin panel)

--- coffee_site/profile/admin.py
+++ coffee_site/profile/admin.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django import forms
 
 from .models import CoffeeUser
 
@@ -10,10 +11,37 @@ from .models import CoffeeUser
 #     model = CoffeeUser
 #     verbose_name_plural = 'coffeeusers'
 
+class CoffeeUserForm(forms.ModelForm):
+
+    class Meta:
+        model = CoffeeUser
+
 # Define a new User admin
 class CoffeeUserAdmin(UserAdmin):
+
+    form = CoffeeUserForm
+
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'ratio_sharing')
 
+    fieldsets = ((None, {'fields': ('username', 'password')}),
+                 ('Personal info',
+                   {'fields': ('first_name', 'last_name', 'email', 'skill_level',)}),
+                 ('Permissions',
+                   {'fields': ('is_active',
+                               'is_staff',
+                               'is_superuser',
+                               'groups',
+                               'user_permissions')}),
+                 ('Important dates',
+                   {'fields': ('last_login', 'date_joined')}),
+
+                 ('Social media',
+                  {'fields': ('facebook', 'twitter', 'gplus')}),
+                 ('Sharing',
+                  {'fields': ('purch_coffee_bag_sharing', 'ratio_sharing', 'friends')}))
+
+    filter_horizontal = ('friends', 'groups')
+
 # # Re-register UserAdmin
-# admin.site.unregister(User)
 admin.site.register(CoffeeUser, CoffeeUserAdmin)
+    
