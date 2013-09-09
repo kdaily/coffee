8d3b7e7249c759ff4e64c282e4b195cda23ba754
Kenneth Daily <kmdaily@gmail.com>
2013-09-08 21:48:48 -0400

    Updated comments.

--- coffee_site/profile/models.py
+++ coffee_site/profile/models.py
@@ -5,10 +5,7 @@ from django.conf import settings
 
 class CoffeeUser(AbstractUser):
     """Subclass user to add extra fields.
-
-    Only extra fields now are to determine what objects (purchased coffee bags
-    and ratio preparations for now) the user wants to share.
-
+    
     """
     
     facebook  = models.CharField(max_length=500, blank=True, null=True)
