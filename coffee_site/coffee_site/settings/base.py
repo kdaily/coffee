4bfa05f6eb8ac498f306a93c6a70945b3f9b03d1
Kenneth Daily <kmdaily@gmail.com>
2013-10-01 22:27:42 -0400

    Added loggers

--- coffee_site/coffee_site/settings/base.py
+++ coffee_site/coffee_site/settings/base.py
@@ -111,6 +111,7 @@ TEMPLATE_LOADERS = (
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
@@ -207,6 +208,11 @@ LOGGING = {
 
     'loggers': {
         'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'INFO',
+            },
+        'views': {
             'handlers': ['console'],
             'propagate': True,
             'level': 'DEBUG',
