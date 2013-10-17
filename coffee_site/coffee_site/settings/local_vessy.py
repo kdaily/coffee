552d17e71c38f0759eebe7e63ffcb41d50141918
Kenneth Daily <kmdaily@gmail.com>
2013-10-16 21:30:51 -0400

    Changed db backend to postgresql.

--- coffee_site/coffee_site/settings/local_vessy.py
+++ coffee_site/coffee_site/settings/local_vessy.py
@@ -7,13 +7,24 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = True #DEBUG
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'djangocoffee',     # Or path to database file if using sqlite3.
+#         'USER': 'dailykm',                      # Not used with sqlite3.
+#         'PASSWORD': '2Sr3x99g',                  # Not used with sqlite3.
+#         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+#         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'djangocoffee',     # Or path to database file if using sqlite3.
-        'USER': 'dailykm',                      # Not used with sqlite3.
-        'PASSWORD': '2Sr3x99g',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'USER': 'coffeesite',                      # Not used with sqlite3.
+        'PASSWORD': '1234',                  # Not used with sqlite3.
+        'HOST': '192.168.1.4',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
